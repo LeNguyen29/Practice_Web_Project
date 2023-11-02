258db05d12211b71f44b76dd70d1f5e402ba34f7
Lễ Nguyễn <nguyenbale29@gmail.com>
2023-11-02 22:38:29 +0700

    Add controller folder because I forgot to put in commit

---
+++ PracticeWebProject/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PracticeWebProject.Models;
+using PracticeWebProject.Services;
+
+namespace PracticeWebProject.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public JsonFileProductService ProductService { get; }
+
+        public ProductsController(JsonFileProductService productService) 
+        { 
+            this.ProductService = productService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Product> GetProducts()
+        {
+            return ProductService.GetProducts();
+        }
+    }
+}
